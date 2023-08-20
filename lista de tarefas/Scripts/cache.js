f07ffef18df3cd90703c43de210ee51cd36723a6
Vinicius <73265565+Vitor0001@users.noreply.github.com>
2023-08-19 21:31:11 -0300

    Correção de bugs
    No último commint ainda havia bugs em relação ao menu de carreganto.

--- lista de tarefas/Scripts/cache.js
+++ lista de tarefas/Scripts/cache.js
@@ -1,46 +1,41 @@
-var nome_lista = document.getElementById('nome')
+var nome = document.getElementById('nome')
 var load = document.getElementById('load')
 var saves = document.getElementById('saves')
 
-nome_lista.addEventListener('keydown', e=>{
-   if(nome_lista.value.trim() == ''){
-      nome_lista.style.backgroundColor = 'lightcoral'
+function carregar(){
+   saves.innerHTML = localStorage.getItem('salvamentos')
+}
+function salvar(){
+   if(nome.value.trim() != '' && lista.innerText != ''){
+      let Slista = document.createElement('p')
+      document.body.appendChild(Slista)
+      saves.appendChild(Slista)
+      Slista.innerText = nome.value.trim()
+      Slista.setAttribute('class', 'salves')
+      
+      localStorage.setItem(nome.value.trim(), lista.innerHTML)
+      localStorage.setItem('salvamentos', saves.innerHTML)
+      carregar()
+   }
+}
+nome.addEventListener('keydown', e=>{
+   if(nome.value.trim() == ''){
+      nome.style.backgroundColor = 'lightcoral'
       document.getElementById('save').style.backgroundColor = 'var(--cor-da-lista)'
    }else{
-      nome_lista.style.backgroundColor =  'var(--cor-da-lista)'
+      nome.style.backgroundColor =  'var(--cor-da-lista)'
        document.getElementById('save').style.backgroundColor = 'lightgreen'   
    }
 })
-function salvar(){
-   if(localStorage.length < 11 && nome_lista.value.trim() != '' && lista.innerText != ''){
-      window.alert('Salvo!')   
-      let save = document.createElement('p')
-      document.body.appendChild(save)
-      saves.appendChild(save)
-
-      localStorage.setItem(nome_lista.value.trim(), lista.innerHTML)
-      save.innerText = localStorage.key(0)
-      localStorage.setItem('salvamentos', saves.innerHTML)
-      nome_lista.value = ''
-   }
-   if(lista.innerText == ''){
-      alert('lista vazia')
-   }
-   if(nome_lista.innerText.trim() == ''){
-      alert('De um nome para sua lista')
-   }
-}
 load.addEventListener('click', e=>{
    if(e.target.tagName == 'P'){
       lista.innerHTML = localStorage.getItem(e.target.innerText)
    }
+})
 load.addEventListener('dblclick', e=>{
-   if(e.target.tagName = 'P'){
+   if(e.target.className == 'salves'){
       localStorage.removeItem(e.target.innerText)
       e.target.remove()
+      localStorage.setItem('salvamentos', saves.innerHTML)
    }
 })
-})
-function carregar(){
-   saves.innerHTML = localStorage.getItem('salvamentos')
-}
